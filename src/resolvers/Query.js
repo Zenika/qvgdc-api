3688d73ddfca97bf85205ddc9b71d2683ccc2696
Nicolas Leport <nicolas.leport@nkcreation.com>
2019-11-28 17:54:19 +0100

    auto increment order questions

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -13,6 +13,8 @@ async function games(parent, args, context, info) {
 }
 
 async function questions(parent, args, context, info) {
+  const userId = getUserId(context);
+
   const questions = await context.prisma.game({ id: args.gameId }).questions();
 
   return questions;
