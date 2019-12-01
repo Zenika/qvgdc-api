53f13809fc350ad60132ade5a39daf0f3f461926
Nicolas Leport <nicolas.leport@nkcreation.com>
2019-12-01 22:15:50 +0100

    order by

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -15,7 +15,9 @@ async function games(parent, args, context, info) {
 async function questions(parent, args, context, info) {
   const userId = getUserId(context);
 
-  const questions = await context.prisma.game({ id: args.gameId }).questions();
+  const questions = await context.prisma.game({ id: args.gameId }).questions({
+    orderBy: "order_ASC"
+  });
 
   return questions;
 }
