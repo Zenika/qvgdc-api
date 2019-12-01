432c9c8d2aef5f68b2112f3bfdf8b6ad2402d803
Nicolas Leport <nicolas.leport@nkcreation.com>
2019-12-01 10:39:40 +0100

    add secret

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -32,6 +32,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://app-097320b7-f47c-4ebf-a1e8-bbb6f0547850.cleverapps.io/`
+  endpoint: `https://app-097320b7-f47c-4ebf-a1e8-bbb6f0547850.cleverapps.io/`,
+  secret: `${process.env["API_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
