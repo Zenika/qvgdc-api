ef8261734cc69e6daf152790bb32c4cec2b91893
Nicolas Leport <nicolas.leport@nkcreation.com>
2019-12-01 22:56:23 +0100

    updateD game

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,16 +1,16 @@
-function updateGameSubscribe(parent, args, context, info) {
+function updatedGameSubscribe(parent, args, context, info) {
   return context.prisma.$subscribe.game({ mutation_in: ["UPDATED"] }).node({
     id: args.gameId
   });
 }
 
-const updateGame = {
-  subscribe: updateGameSubscribe,
+const updatedGame = {
+  subscribe: updatedGameSubscribe,
   resolve: payload => {
     return payload;
   }
 };
 
 module.exports = {
-  updateGame
+  updatedGame
 };
