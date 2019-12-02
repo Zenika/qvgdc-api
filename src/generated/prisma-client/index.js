7c9c8d1d9cb21ce6e0fc0f8ea29a21d6cfbf0906
Nicolas Leport <nicolas.leport@nkcreation.com>
2019-12-02 22:29:07 +0100

    :bug: forgot to remove

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -32,7 +32,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://app-097320b7-f47c-4ebf-a1e8-bbb6f0547850.cleverapps.io/`,
-  secret: `quiveutgagnerdescanards`
+  endpoint: `${process.env["API_URL"]}`,
+  secret: `${process.env["API_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
